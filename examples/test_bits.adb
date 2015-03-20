a47a5b13e9f7992e5549ed0170169227beeab54b
James Humphry <git@binecho.co.uk>
2015-03-20 21:26:23 +0000

    Add Parse_Args command line options to test_bits
    The example test program test_bits now uses the Parse_Args package to
    allow it to take standard command line arguments to specify the desired
    seed and number of iterations.

--- examples/test_bits.adb
+++ examples/test_bits.adb
@@ -13,10 +13,16 @@ use all type PRNG_Zoo.Linear_Congruential.Examples.MINSTD;
 
 with PRNG_Zoo.Tests, PRNG_Zoo.Tests.Bits, PRNG_Zoo.Tests.EquiDist;
 
+with Parse_Args;
+
 with Ada.Text_IO;
 use Ada.Text_IO;
 
 procedure test_bits is
+   AP : Parse_Args.Argument_Parser;
+   Seed : PRNG_Zoo.U64;
+   Iterations : Natural;
+
    G1 : xorshift_star.xorshift1024_star;
    G2 : Linear_Congruential.Examples.RANDU;
    G3 : Linear_Congruential.Examples.MINSTD;
@@ -27,10 +33,30 @@ procedure test_bits is
    ED_31 : Tests.EquiDist.EquiDist(2,2,31);
 begin
 
-   G1.Reset(9753);
+   AP.Set_Prologue("A simple demonstration of the Parse_Args library.");
+   AP.Add_Option(Parse_Args.Make_Boolean_Option(False), "help", 'h',
+                 Usage => "Display this help text");
+   AP.Add_Option(Parse_Args.Make_Natural_Option(9753), "seed", 's',
+                 Usage => "Specify a seed for the generators");
+   AP.Add_Option(Parse_Args.Make_Natural_Option(1), "iterations", 'i',
+                 Usage => "Specify iterations (in millions)");
+   AP.Parse_Command_Line;
+
+   if not AP.Parse_Success then
+      Put_Line("Error while parsing command-line arguments: " & AP.Parse_Message);
+      goto Finish;
+   elsif AP.Boolean_Value("help") then
+      AP.Usage;
+      goto Finish;
+   end if;
+
+   Seed := PRNG_Zoo.U64(AP.Natural_Value("seed"));
+   Iterations := AP.Natural_Value("iterations") * 1_000_000;
+
+   G1.Reset(Seed);
    BC.Reset; ED_64.Reset;
    Put_Line("Testing xorshift1024*");
-   for I in 1..1_000_000 loop
+   for I in 1..Iterations loop
       X := G1.Generate;
       BC.Feed(X);
       ED_64.Feed(X);
@@ -52,10 +78,10 @@ begin
 
    Put_Line(ED_64.Describe_Result); New_Line;
 
-   G2.Reset(9753);
+   G2.Reset(Seed);
    BC_31.Reset; ED_31.Reset;
    Put_Line("Testing RANDU");
-   for I in 1..1_000_000 loop
+   for I in 1..Iterations loop
       X := U64(U32'(G2.Generate));
       BC_31.Feed(X);
       ED_31.Feed(X);
@@ -77,10 +103,10 @@ begin
 
    Put_Line(ED_31.Describe_Result); New_Line;
 
-   G3.Reset(9753);
+   G3.Reset(Seed);
    BC_31.Reset; ED_31.Reset;
    Put_Line("Testing MINSTD");
-   for I in 1..1_000_000 loop
+   for I in 1..Iterations loop
       X := U64(U32'(G3.Generate));
       BC_31.Feed(X);
       ED_31.Feed(X);
@@ -102,4 +128,6 @@ begin
 
    Put_Line(ED_31.Describe_Result); New_Line;
 
+   <<Finish>>
+   null;
 end test_bits;
