247afc8fa41cc8943e73fc2f8c827df94d316413
James Humphry <git@binecho.co.uk>
2015-03-23 17:29:21 +0000

    Allow multiple PRNG names to be specified
    More than one PRNG name can now be specified on the command line for the
    test_bits example. This code has been split out into a separate unit as
    it is expected to be re-usable.

---
+++ examples/common_cli.adb
@@ -0,0 +1,55 @@
+--
+-- PRNG Zoo
+-- Copyright 2014 - 2015 James Humphry
+--
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+with Ada.Containers;
+use type Ada.Containers.Count_Type;
+
+with Parse_Args;
+with PRNG_Zoo.Register;
+use PRNG_Zoo.Register;
+
+procedure Common_CLI(AP : in out Parse_Args.Argument_Parser;
+                     PRNG_Names : in out Parse_Args.String_Doubly_Linked_Lists.List) is
+   I : Register_Cursor;
+begin
+
+   PRNG_Names := Parse_Args.String_Doubly_Linked_Lists.Empty_List;
+
+   AP.Add_Option(Parse_Args.Make_Boolean_Option(False), "help", 'h',
+                 Usage => "Display this help text");
+   AP.Add_Option(Parse_Args.Make_Boolean_Option(False), "list-prng", 'p',
+                 Usage => "List available PRNG");
+   AP.Allow_Tail_Arguments;
+
+   AP.Parse_Command_Line;
+
+   if not AP.Parse_Success then
+      Put_Line("Error while parsing command-line arguments: " & AP.Parse_Message);
+   elsif AP.Boolean_Value("help") then
+      AP.Usage;
+   elsif AP.Boolean_Value("list-prng") then
+      PRNG_Zoo.Register.Display_Register;
+   elsif AP.Tail.Length = 0 then
+      Put_Line("At least one PRNG must be specified.");
+   elsif AP.Tail.First_Element = "all" then
+      for I in Register.Iterate loop
+         PRNG_Names.Append (PRNG_Registries.Key (I));
+      end loop;
+   else
+      for J of AP.Tail loop
+         if not PRNG_Zoo.Register.PRNG_Exists(J) then
+            PRNG_Names := Parse_Args.String_Doubly_Linked_Lists.Empty_List;
+            Put_Line("No such PRNG: '" & J & "'");
+            exit;
+         end if;
+         PRNG_Names.Append (J);
+      end loop;
+
+   end if;
+
+end Common_CLI;
--- examples/test_bits.adb
+++ examples/test_bits.adb
@@ -12,11 +12,14 @@ with PRNG_Zoo.Tests, PRNG_Zoo.Tests.Bits, PRNG_Zoo.Tests.EquiDist;
 
 with Parse_Args;
 
+with Common_CLI;
+
 with Ada.Text_IO;
 use Ada.Text_IO;
 
 procedure test_bits is
    AP : Parse_Args.Argument_Parser;
+   PRNG_Names : Parse_Args.String_Doubly_Linked_Lists.List;
    Seed : PRNG_Zoo.U64;
    Iterations : Natural;
    X : U64;
@@ -24,10 +27,7 @@ procedure test_bits is
 begin
 
    AP.Set_Prologue("Test bit distributions of PRNG.");
-   AP.Add_Option(Parse_Args.Make_Boolean_Option(False), "help", 'h',
-                 Usage => "Display this help text");
-   AP.Add_Option(Parse_Args.Make_Boolean_Option(False), "list-prng", 'p',
-                 Usage => "List available PRNG");
+
    AP.Add_Option(Parse_Args.Make_Natural_Option(9753), "seed", 's',
                  Usage => "Specify a seed for the generators (default 9753)");
    AP.Add_Option(Parse_Args.Make_Natural_Option(1), "iterations", 'i',
@@ -38,62 +38,47 @@ begin
                  Usage => "Log_{2} Dimensionality of equidistribution test (default 2)");
    AP.Add_Option(Parse_Args.Make_Natural_Option(2), "divisions", 'l',
                  Usage => "Log_{2} Divisions in each dimension (default 2)");
-   AP.Append_Positional(Parse_Args.Make_String_Option(""), "PRNG");
-   AP.Parse_Command_Line;
 
-   if not AP.Parse_Success then
-      Put_Line("Error while parsing command-line arguments: " & AP.Parse_Message);
-      goto Finish;
-   elsif AP.Boolean_Value("help") then
-      AP.Usage;
-      goto Finish;
-   elsif AP.Boolean_Value("list-prng") then
-      PRNG_Zoo.Register.Display_Register;
-      goto Finish;
-   elsif AP.String_Value("PRNG") = "" then
-      Put_Line("A PRNG must be specified.");
-      goto Finish;
-   elsif not PRNG_Zoo.Register.PRNG_Exists(AP.String_Value("PRNG")) then
-      Put_Line("No such PRNG: '" & AP.String_Value("PRNG") & "'");
-      goto Finish;
-   end if;
+   Common_CLI(AP, PRNG_Names);
 
    Seed := PRNG_Zoo.U64(AP.Natural_Value("seed"));
    Iterations := AP.Natural_Value("iterations") * 1_000_000;
 
-   declare
-      G : PRNG'Class := Register.Make_PRNG(AP.String_Value("PRNG"));
-      BC : Tests.Bits.Bit_Counter(AP.Natural_Value("output-width"));
-      ED : Tests.EquiDist.EquiDist(t => AP.Natural_Value("dimensions"),
-                                   l => AP.Natural_Value("divisions"),
-                                   n => AP.Natural_Value("output-width"));
-   begin
-      G.Reset(Seed);
-      BC.Reset;
-      ED.Reset;
-      Put_Line("Testing " & AP.String_Value("PRNG"));
+   for Name of PRNG_Names loop
+
+      declare
+         G : PRNG'Class := Register.Make_PRNG(Name);
+         BC : Tests.Bits.Bit_Counter(AP.Natural_Value("output-width"));
+         ED : Tests.EquiDist.EquiDist(t => AP.Natural_Value("dimensions"),
+                                      l => AP.Natural_Value("divisions"),
+                                      n => AP.Natural_Value("output-width"));
+      begin
+         G.Reset(Seed);
+         BC.Reset;
+         ED.Reset;
+         Put_Line("Testing " & Name);
+         New_Line;
+
+         for I in 1..Iterations loop
+            X := G.Generate;
+            BC.Feed(X);
+            ED.Feed(X);
+         end loop;
+
+         BC.Compute_Result;
+         Put_Line(Name &
+                  (if BC.Passed then " passed " else " failed " ) &
+                    "Bit-Counter test:");
+         Put_Line(BC.Describe_Result); New_Line;
+
+         ED.Compute_Result;
+         Put_Line(Name &
+                  (if ED.Passed then " passed " else " failed " ) &
+                    "Equidistribution test:");
+         Put_Line(ED.Describe_Result); New_Line;
+
+      end;
       New_Line;
+   end loop;
 
-      for I in 1..Iterations loop
-         X := G.Generate;
-         BC.Feed(X);
-         ED.Feed(X);
-      end loop;
-
-      BC.Compute_Result;
-      Put_Line(AP.String_Value("PRNG") &
-               (if BC.Passed then " passed " else " failed " ) &
-                 "Bit-Counter test");
-      Put_Line(BC.Describe_Result); New_Line;
-
-      ED.Compute_Result;
-      Put_Line(AP.String_Value("PRNG") &
-               (if ED.Passed then " passed " else " failed " ) &
-                 "Equidistribution test");
-      Put_Line(ED.Describe_Result); New_Line;
-
-   end;
-
-   <<Finish>>
-   null;
 end test_bits;
